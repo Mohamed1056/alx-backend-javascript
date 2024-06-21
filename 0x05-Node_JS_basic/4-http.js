8b4d10a878fb0ee51c713a3c18c04a1471a8ab07
mg8033667@gmail.com <Mohamed Gamal>
2024-06-21 15:26:11 -0700

    adding 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
-const port = 1245;
-const host = '127.0.0.1';
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
 
-const app = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader = ('content-type', 'text/plain');
-	res.end('Hello Holberton School!');
-})
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
 
-app.listen(port, host)
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
